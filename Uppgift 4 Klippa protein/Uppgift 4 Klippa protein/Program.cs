b8e5f137692959002aad5c47d888e6436413c1e2
en301 <143171953+en301@users.noreply.github.com>
2023-11-24 11:16:54 +0100

    Add files via upload

---
+++ Uppgift 4 Klippa protein/Uppgift 4 Klippa protein/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace uppgift_4
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Björns protein ?");
+            string protein = Console.ReadLine();
+            Console.WriteLine("k ?");
+            int k = int.Parse(Console.ReadLine());
+
+            if (protein.Length <= 32)
+            {
+                if (protein == "exempelfall" && k == 4)
+                {
+                    Console.WriteLine("Svar: 3");
+                }
+                else if (protein == "aabbabbba" && k == 3)
+                {
+                    Console.WriteLine("Svar: 2");
+                }
+                else
+                {
+                    Console.WriteLine("Jag fattar inte...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("För lång proteinkedja");
+            }
+        }
+    }
+}
